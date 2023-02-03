ed238b4b6cf8558b0991e98196880d51a4bbee57
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-02-03 19:07:31 -0300

    feat(server.js): salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
-  if(method === 'GET' && url === '/user'){
-    return res.end('Listagem de usuários')
+  if(method === 'GET' && url === '/users'){
+    return res
+    .setHeader('Content-Type', 'application/json')
+    .end(JSON.stringify(users))
   }
 
-  if(method === 'POST' && url === '/user'){
-    return res.end('Usuário adicionado')
+  if(method === 'POST' && url === '/users'){
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@exemple.com'
+    })
+    return res.end(`Usuário adicionado`)
   }
 
  return res.end('Hello world')
