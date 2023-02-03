9549461000bdd9e85b5d1b711263f571837917bc
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-02-02 21:51:18 -0300

    feat(server.js): rotas de criação e listagem (Métodos HTTP)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import http from 'node:http'
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if(method === 'GET' && url === '/user'){
+    return res.end('Listagem de usuários')
+  }
+
+  if(method === 'POST' && url === '/user'){
+    return res.end('Usuário adicionado')
+  }
+
  return res.end('Hello world')
 })
 
