aa8b6cd91d014cbd47fc3b88d1450924eecb89fe
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-03-07 19:27:17 -0300

    feat(stream-http-server.js): aplicando Streams no módulo HTTP

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -1,21 +1,23 @@
-import http from 'node:http';
+import http from 'node:http'
 import { Transform } from 'node:stream'
 
+ class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback){
+    const negativeNumber = Number(chunk.toString() * -1)
 
-class InverseNumberStream extends Transform {
-  _transform(chunk, encoding, callback) {
-    const transformed = Number(chunk.toString()) * -1
+    console.log(negativeNumber)
 
-    console.log(transformed)
-
-    callback(null, Buffer.from(String(transformed)))
+    callback(null, Buffer.from(String(negativeNumber)))
   }
 }
 
 const server = http.createServer((req, res) => {
-  return req
+ return req
   .pipe(new InverseNumberStream())
   .pipe(res)
-})
+
+  res.writeHead(201).end()
+
+} )
 
 server.listen(3334)
