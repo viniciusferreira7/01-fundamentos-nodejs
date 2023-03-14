d3465e09b6a50c6ddb4abb6982546b71f7bafc89
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-03-14 20:27:17 -0300

    feat(server.js): criando ID único e universal (UUID)

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
+
 import { Database } from './database.js'
 import { json } from './middleware/json.js'
 
@@ -20,7 +22,7 @@ const server = http.createServer( async (req, res) => {
     const { name, email } = req.body
 
     const user = {
-      id: 1,
+      id: randomUUID(),
       name, 
       email
     }
